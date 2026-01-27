3f160ddac7e9975d98f1417bc38adceffd8e93fc
vivnasc <61882995+vivnasc@users.noreply.github.com>
2026-01-27 12:04:12 +0200

    Update ReceitasBrowse.jsx

--- src/components/vitalis/ReceitasBrowse.jsx
+++ src/components/vitalis/ReceitasBrowse.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../../lib/supabase.js';
 import { Link } from 'react-router-dom';
 
 export const ReceitasBrowse = () => {
