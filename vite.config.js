0aafa95a90a3bdfec85a744716006ac347d8ef24
vivnasc <61882995+vivnasc@users.noreply.github.com>
2026-01-30 20:42:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,26 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  server: { port: 3000 },
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      zlib: 'browserify-zlib',
+      util: 'util',
+      buffer: 'buffer',
+    }
+  },
+  optimizeDeps: {
+    include: ['@react-pdf/renderer'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
